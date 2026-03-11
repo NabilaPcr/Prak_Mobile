d178814409a3b60b82914c8965085c272715c028
lab151-30 <dzikri24si@mahasiswa.pcr.ac.id>
2026-03-11 09:35:02 +0700

    first commit mobile

---
+++ app/src/main/java/com/example/bellapps/pertemuan_2/BelajarKotlin.kt
@@ -0,0 +1,44 @@
+package com.example.bellapps.pertemuan_2
+
+fun main() {
+    println("Hai rekan rekan...")
+    println("Selamat datang di bahasa pemograman Kotlin")
+
+    println("====================================")
+
+    var angka = 15
+    println("Hasil dari 15 + 10 = ${angka + 10}")
+
+    val nilaiInt = 10000
+    val nilaiDouble = 100.003
+    val nilaiFloat = 100.0f
+
+    println("Nilai Integer = $nilaiInt")
+    println("Nilai Double = $nilaiDouble")
+    println("Nilai Float = $nilaiFloat")
+
+    println("========String=========")
+    val huruf = 'a'
+    println("Ini penggunaan karakter '$huruf' ")
+
+    val nilaiString = "Mawar"
+    println("Halo $nilaiString!\nApa kabar?")
+
+    println("==========KONDISI===========")
+
+    val nilai = 10
+    if (nilai<0) {
+        println("Bilangan Negatif")
+    } else {
+        if (nilai%2 == 0) {
+            println("Bilangan Genap")
+        } else {
+            println("Bilangan Ganjil")
+        }
+    }
+    println("==========Perulangan==========")
+    val kampusKu: Array<String> = arrayOf("Kampus ", "Politeknik ", "Caltex ", "Riau")
+    for (kampus: String in kampusKu) {
+        print(kampus)
+    }
+}
