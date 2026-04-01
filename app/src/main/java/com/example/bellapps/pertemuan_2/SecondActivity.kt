d753252e11ce0778bc05ba9b16b3b9d714fc7408
lab151-30 <lab151@gmail.com>
2026-04-01 07:22:07 +0700

    sec commit

--- app/src/main/java/com/example/bellapps/pertemuan_2/SecondActivity.kt
+++ app/src/main/java/com/example/bellapps/pertemuan_2/SecondActivity.kt
@@ -27,7 +27,7 @@ class SecondActivity : AppCompatActivity() {
         val btnSubmit: Button = findViewById(R.id.btnSubmit)
 
 
-        //Event OnClick, saat ditekan akan mengeluarkan pop up
+        //Event OnClick, saat ditekan akan mengeluarkan pop up bahwa tombol; sudah di klik
         btnSubmit.setOnClickListener {
             //Mengambil value dari inputNama dan menampilkan di Logcat
             val nama = inputNama.text
